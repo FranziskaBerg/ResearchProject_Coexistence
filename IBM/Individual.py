be8ea5b9837f3584d40f69f6e5120d603febbca5
FranziskaBerg <bergfranziska@live.de>
2014-12-01 12:20:27 +0100

    Offspring from Poisson added

--- IBM/Individual.py
+++ IBM/Individual.py
@@ -4,6 +4,7 @@ Created on 27.11.2014
 @author: Franziska Berg
 '''
 import random
+import numpy as np
 
 class Individual:
     
@@ -11,8 +12,9 @@ class Individual:
         self.number = 1
         self.b_value = random.uniform(0.17,15)
         
-    def getOffspring(self,r,d,K):
-        self.number += self.number*r*(1-d)/(1+(r-1)*(self.number/K)**self.b_value)
+    def getOffspring(self,r,d,Nto,Kto):
+        poisson = r*(1-d)/(1+(r-1)*(Nto/Kto)**self.b_value)
+        self.number += np.random.poisson(poisson)
         return self
     
     def getIndividualNumber(self):
