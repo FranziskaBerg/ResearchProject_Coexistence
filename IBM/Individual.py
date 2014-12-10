c328eb7619895a755e7074abc71c48460bd17510
FranziskaBerg <bergfranziska@live.de>
2014-12-10 10:46:58 +0100

    Classes for Model: Individual and Population

--- IBM/Individual.py
+++ IBM/Individual.py
@@ -3,19 +3,32 @@ Created on 27.11.2014
 
 @author: Franziska Berg
 '''
-import random
 import numpy as np
+from copy import deepcopy
 
 class Individual:
     
     def __init__(self):
         self.number = 1
-        self.b_value = random.uniform(0.17,15)
+        self.b_value = np.random.normal(0,15)
+        if self.b_value < 0.17:
+            self.b_value = 0.17
         
-    def getOffspring(self,r,d,Nto,Kto):
-        poisson = r*(1-d)/(1+(r-1)*(Nto/Kto)**self.b_value)
-        self.number += np.random.poisson(poisson)
-        return self
+    def reproduce(self,r,d,N,K):
+        poisson = self.number*r*(1-d)/(1+(r-1)*(N/K)**self.b_value)
+        self.number = np.random.poisson(poisson)
     
+    def getMutant(self):
+        self.number -= 1
+        newMutant = deepcopy(self)
+        newMutant.number = 1
+        newMutant.b_value = self.b_value + np.random.normal()
+        if newMutant.b_value < 0.17:
+            newMutant.b_value = 0.17
+        return(newMutant)
+        
     def getIndividualNumber(self):
         return self.number
+    
+
+            
--- IBM/Population.py
+++ IBM/Population.py
@@ -4,31 +4,42 @@ Created on 27.11.2014
 @author: Franziska Berg
 '''
 from Individual import *
+import numpy as np
 
 class Population:
     
-    def __init__(self,N,r,d,K):
+    def __init__(self,N,r,d,K,m):
         self.individuals = []
+        self.N = 0
         self.r = r
         self.d = d
         self.K = K
-        for individual in range(1,N+1):
-            individual = Individual()
-            self.individuals += [individual,]
-        print self.individuals
+        self.m = m
+        for i in range(0,N):
+            self.individuals.append(Individual())
     
-    def updatePopulation(self):
-        N = len(self.individuals)
-        Nto = self.getPopulationSize()
-        for i in range(0,N):
-            individual = self.individuals[i]
-            individual = individual.getOffspring(self.r,self.d,Nto,self.K)
-            self.individuals[i] = individual
-            
+    def updatePopulation(self,cycle):
+        for c in range (0,cycle):
+            self.getPopulationSize()
+            for i in self.individuals:
+                i.reproduce(self.r, self.d, self.N, self.K)
+            self.mutate()
+            print self.getPopulationSize()
+        
+    def mutate(self):
+        for i in self.individuals:
+            numberIndividuals = i.getIndividualNumber()
+            numberMutants = np.random.poisson(numberIndividuals *self.m / self.K)
+            if numberMutants > numberIndividuals: numberMutants = numberIndividuals
+            for k in range(0,numberMutants):
+                mutant = i.getMutant()
+                if i.getIndividualNumber <= 0:
+                    self.individuals.remove(i)
+                self.individuals.append(mutant)
+          
     def getPopulationSize(self):
-        N = len(self.individuals)
         individualN = 0
-        for i in range(0,N):
-            individual = self.individuals[i]
-            individualN += individual.getIndividualNumber()
-        return individualN
+        for i in self.individuals:
+            individualN += i.getIndividualNumber()
+        self.N = individualN
+        return self.N
